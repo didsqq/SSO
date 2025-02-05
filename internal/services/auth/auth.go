9d1f055878a6a8080bc2053b490280a21c774a9c
didsqq <adelgaripov6@gmail.com>
2025-02-05 22:54:56 +0300

    auth refact

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -7,6 +7,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	"log/slog"
 	"sso/internal/domain/models"
+	"sso/internal/lib/jwt"
 	"sso/internal/storage"
 	"time"
 )
@@ -44,14 +45,15 @@ func New(log *slog.Logger, userSave UserSave, userProvider UserProvider, appProv
 
 var (
 	ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrUserExists         = errors.New("user already exists")
 )
 
 func (a *Auth) Login(ctx context.Context, email, password string, appID int) (string, error) {
 	const op = "auth.Login"
 
 	log := a.log.With(
-		slog.String("email", op),
-		slog.String("email", email),
+		slog.String("op", op),
+		slog.String("username", email),
 	)
 
 	log.Info("registering new user")
@@ -73,18 +75,26 @@ func (a *Auth) Login(ctx context.Context, email, password string, appID int) (st
 
 	app, err := a.appProvider.App(ctx, appID)
 	if err != nil {
-		return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+		return "", fmt.Errorf("%s: %w", op, err)
 	}
 
 	log.Info("user logged in successfully")
 
+	token, err := jwt.NewToken(user, app, a.tokenTTL)
+	if err != nil {
+		a.log.Error("failed to generate token", slog.String("error", err.Error()))
+
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return token, nil
 }
 
 func (a *Auth) RegisterNewUser(ctx context.Context, email, password string) (int64, error) {
 	const op = "auth.RegisterNewUser"
 
 	log := a.log.With(
-		slog.String("email", op),
+		slog.String("op", op),
 		slog.String("email", email),
 	)
 
@@ -99,6 +109,11 @@ func (a *Auth) RegisterNewUser(ctx context.Context, email, password string) (int
 
 	id, err := a.userSave.SaveUser(ctx, email, passHash)
 	if err != nil {
+		if errors.Is(err, storage.ErrUserExists) {
+			log.Warn("user already exists")
+
+			return 0, ErrUserExists
+		}
 		log.Error("failed to save user", slog.String("err", err.Error()))
 
 		return 0, fmt.Errorf("%s: %w", op, err)
@@ -108,5 +123,19 @@ func (a *Auth) RegisterNewUser(ctx context.Context, email, password string) (int
 }
 
 func (a *Auth) IsAdmin(ctx context.Context, userID int64) (bool, error) {
-	panic("implement me")
+	const op = "auth.IsAdmin"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("userID", fmt.Sprint(userID)),
+	)
+
+	log.Info("checking if user is admin")
+
+	isAdmin, err := a.userProvider.IsAdmin(ctx, userID)
+	if err != nil {
+		return false, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return isAdmin, nil
 }
