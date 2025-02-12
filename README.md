bcbfdb04010299cb72474ee3b259b3af7a5b9dbe
didsqq <adelgaripov6@gmail.com>
2025-02-12 22:52:35 +0300

    1 test work

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SSO
+go run ./cmd/migrator/main.go --storage-path=./storage/sso.db --migrations-path=./tests/migrations --migrations-table=migrations_test
--- internal/config/config.go
+++ internal/config/config.go
@@ -26,13 +26,17 @@ func MustLoad() *Config { // Must - значит не обрабатываем �
 		panic("config path is empty")
 	}
 
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		panic("config file does not exist: " + path)
+	return MustLoadByPath(path)
+}
+
+func MustLoadByPath(configPath string) *Config { // Must - значит не обрабатываем ошибку, просто паникуем
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic("config file does not exist: " + configPath)
 	}
 
 	var cfg Config
 
-	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
 		panic("failed to read config: " + err.Error())
 	}
 
